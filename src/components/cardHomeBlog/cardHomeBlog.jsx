88d00a5d1ff7ede1f5a34d5de56075c39432e0d1
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-07-25 12:05:26 +0500

    fixed

--- src/components/cardHomeBlog/cardHomeBlog.jsx
+++ src/components/cardHomeBlog/cardHomeBlog.jsx
@@ -1,8 +1,8 @@
 const CardHomeBlog = () => {
   return (
-    <div className="card-content relative -z-[1] max-w-[360px] justify-center xl:justify-normal xl:max-w-max max-h-[610px] ">
-      <img src="../../../public/Frame 267.png" className="img absolute top-0 left-0 h-full w-full bg-[#C4D3EA] rounded-[17px] -z-[2]"></img>
-      <div className="btm bgh rounded-[17px] border-none min-h-[460px] flex flex-col justify-end p-[26px]">
+    <div className="card-content relative max-w-[360px] justify-center xl:justify-normal xl:max-w-max max-h-[610px] ">
+      <div className="img absolute top-0 left-0 h-full w-full bg-[#C4D3EA] rounded-[17px] -z-[1]"></div>
+      <div className="btm rounded-[17px] bgh min-h-[460px] flex flex-col justify-end p-[26px]">
         <h4 className="text-[32px] font-inter font-bold text-[#fff]">Клан 1</h4>
         <p className="text-[11px] mt-[22px] font-inter font-[400] text-[#fff]">
           Lorem ipsum dolor sit amet, consec adipiscing elit, sed consec
